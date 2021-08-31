af235967db6ac116a1453c1deef0e60af9c83ca2
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2021-08-31 12:00:45 +0530

    Push New Project

---
+++ JSONAndXMLParseDemo/JSONAndXMLParseDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json;
+namespace JSONAndXMLParseDemo
+{
+    //  This program demo. to convert (parse) json to xml
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonValue = @"{
+            'root':{
+                    'Profile':[
+                                    {'íd':'1','name':'scott','address':'Pune'},
+                                    {'íd':'2','name':'tiger','address':'mumbai'}
+                              ]
+                    }
+            }";
+            //  Represents an XML document. You can use this class to load, validate, edit, add, and position XML in a document.
+            XmlDocument doc = JsonConvert.DeserializeXmlNode(jsonValue);
+            Console.WriteLine(doc.InnerXml);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ JSONAndXMLParseDemo/JSONAndXMLParseDemo/XMLToJsonDemo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace JSONAndXMLParseDemo
+{
+    class XMLToJsonDemo
+    {
+        static void Main()
+        {
+            var xmlValue = "<root><Profile><íd>1</íd><name>scott</name><address>Pune</address></Profile><Profile><íd>2</íd><name>tiger</name><address>mumbai</address></Profile></root>";
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xmlValue);
+            var jsonValue = JsonConvert.SerializeXmlNode(doc);
+            Console.WriteLine(jsonValue);
+            Console.ReadLine();
+        }
+    }
+}
